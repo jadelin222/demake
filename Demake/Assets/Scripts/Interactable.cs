f884c11e0a50f365b02e666d71418d38068c75fe
yong <109802312+jadelin222@users.noreply.github.com>
2024-10-28 11:24:22 +0000

    added models, fog, etc.

---
+++ Demake/Assets/Scripts/Interactable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    public bool isCollectable;
+    public bool haveMultipleLines;
+    public string ControlHint { get; private set; }
+    public string ItemDescription { get; private set; }
+    public abstract void Interact();   //pick, open, close, etc
+    //public abstract void OnRayHit();
+    public virtual void OnRayHit()
+    {
+        GetControlHint();
+        GetItemDescription();
+    }
+
+    public void GetControlHint()
+    {
+        //td: wwhen looking at the 
+    }
+    public void GetItemDescription()
+    {
+        
+    }
+    public void GetMissingInteractionItemHint()
+    {
+
+    }
+
+
+}
